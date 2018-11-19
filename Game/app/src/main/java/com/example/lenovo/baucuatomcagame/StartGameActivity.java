dcb263977eadfdb621903310176297ce20de6a6b
Vy Phan <vywebber@gmail.com>
2018-11-19 23:43:15 +0700

    create project

---
+++ Game/app/src/main/java/com/example/lenovo/baucuatomcagame/StartGameActivity.java
@@ -0,0 +1,13 @@
+package com.example.lenovo.baucuatomcagame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StartGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_game);
+    }
+}
